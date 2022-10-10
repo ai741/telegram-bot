46b93e729349f13af1deec3e43e01523bb225070
Александр Петров <link.4el@gmail.com>
2022-10-10 19:12:49 +0500

    version 1.0.0

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ const token = "5756023407:AAFZegnQT-KKXrvNkU39ZtR37vmPJgWLEkA"
 const bot = new TelegramBot(token, {polling: true})
 
 
+bot.setMyCommands([
+    {command: "/info", description: "Пук-пук"}
+])
+
+const requestKeyboard = {
+    "reply_markup": {
+        "one_time_keyboard": true,
+        "keyboard": [
+            [{
+                text: "Сам пошел",
+                one_time_keyboard: true
+            }],
+        ]
+    }
+};
+
 bot.on("message", msg=>{
     const text = msg.text
     const chatId = msg.chat.id
 
-    bot.sendMessage(chatId, text)
-}) 
+    bot.sendMessage(chatId, 'Пошел нахрен', requestKeyboard);
+
+
+
+})
